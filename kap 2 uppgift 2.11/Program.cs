e34ca17a2237828d815ef91b631cabdac5c14331
Alexsept177 <alka04006@utb.vaxjo.se>
2022-09-07 12:12:33 +0200

    Add project files.

---
+++ kap 2 uppgift 2.11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift_2._11
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur långt hoppade Alma?");
+            string längdText = Console.ReadLine();
+            double längd= double.Parse(längdText);
+            double skillnad = 10.5 - längd;
+
+            Console.WriteLine("Elin hoppade" + 3 + "meterhögre");
+        }
+    }
+}
